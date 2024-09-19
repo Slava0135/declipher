756cbbf8c18caa6367cd0b4b7f696d77fb23f130
slava0135 <super.novalskiy_0135@inbox.ru>
2024-09-19 13:54:09 +0300

    one char encode

--- src/cipher.rs
+++ src/cipher.rs
@@ -8,7 +8,13 @@ impl Cipher<'_> {
     pub fn encode(self, text: &str, key: &str) -> String {
         assert!(text.chars().all(|it| self.text_alphabet.contains(it)));
         assert!(key.chars().all(|it| self.key_alphabet.contains(it)));
-        return String::from(text);
+        if key.is_empty() {
+            return String::from(text);
+        }
+        let text_index = str::find(self.text_alphabet, text.chars().nth(0).unwrap()).unwrap();
+        let key_index = str::find(self.key_alphabet, key.chars().nth(0).unwrap()).unwrap();
+        let combined_index = (text_index + key_index + 1) % self.text_alphabet.chars().count();
+        return String::from(self.text_alphabet.chars().nth(combined_index).unwrap());
     }
 }
 
@@ -32,5 +38,12 @@ mod tests {
     #[test]
     fn encode_empty() {
         assert_eq!("a", DEFAULT_CIPHER.encode("a", ""));
+    }
+
+    #[test]
+    fn encode_one() {
+        assert_eq!("b", DEFAULT_CIPHER.encode("a", "d"));
+        assert_eq!("c", DEFAULT_CIPHER.encode("a", "e"));
+        assert_eq!("a", DEFAULT_CIPHER.encode("a", "f"));
     }
 }
