e26502d634b7123e201b0b30da433c2d0d23fe13
slava0135 <super.novalskiy_0135@inbox.ru>
2024-09-19 15:03:35 +0300

    read words for keys from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use std::io::BufRead;
+
 mod cipher;
 
 fn main() {
-    println!("Hello, world!");
+    let words_file = std::fs::File::open("words.dat").expect("Failed to load file with words 'words.dat'!");
+    let mut words = Vec::new();
+    for word in std::io::BufReader::new(words_file).lines() {
+        if let Ok(word) = word {
+            words.push(word);
+        }
+    }
 }
